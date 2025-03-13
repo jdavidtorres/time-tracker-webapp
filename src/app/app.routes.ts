ecce197df070e58e64a13b8c712f2447215e27a4
Yisus <jesu_ti91@hotmail.com>
2025-03-13 18:29:46 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AuthGuard } from './guards/auth.guard';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-  { path: 'tasks', component: TaskListComponent, canActivate: [AuthGuard] },
-  { path: 'tasks/new', component: TaskFormComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/tasks', pathMatch: 'full' }
+	{ path: 'tasks', component: TaskListComponent, canActivate: [AuthGuard] },
+	{ path: 'tasks/new', component: TaskFormComponent, canActivate: [AuthGuard] },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent },
+	{ path: '', redirectTo: '/tasks', pathMatch: 'full' }
 ];
