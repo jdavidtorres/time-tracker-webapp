29a9ee23a402845cb64d6169d3658bf4e3a94828
Yisus <jesu_ti91@hotmail.com>
2025-03-13 15:44:43 -0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+import Aura from '@primeng/themes/aura';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+	providers: [
+		provideHttpClient(withInterceptorsFromDi()),
+		{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes, withEnabledBlockingInitialNavigation()),
+		provideHttpClient(withFetch()),
+		provideAnimationsAsync(),
+		providePrimeNG({
+			theme: {
+				preset: Aura,
+				options: {
+					darkModeSelector: false || 'none'
+				}
+			}
+		})
+	]
 };
