9844a745eb2110806b9c45f786c013d5712e42fe
Yisus <jesu_ti91@hotmail.com>
2025-03-13 18:52:54 -0500

    Create register.component.ts

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+	selector: 'app-register',
+	imports: [
+		CommonModule,
+		FormsModule,
+		RouterLink,
+		CardModule,
+		FloatLabelModule,
+		InputTextModule,
+		ButtonModule
+	],
+	templateUrl: './register.component.html',
+	styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+	username: string = '';
+	password: string = '';
+	confirmPassword: string = '';
+
+	constructor(private readonly authService: AuthService, private readonly router: Router) { }
+
+	onSubmit() {
+		if (this.password !== this.confirmPassword) {
+			alert('Passwords do not match');
+			return;
+		}
+
+		this.authService.register(this.username, this.password);
+	}
+}
