80553824a4b9694b90e8a7996681028c800de2c2
Yisus <jesu_ti91@hotmail.com>
2025-03-13 16:54:40 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'tasks', component: TaskListComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/new', component: TaskFormComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' }
+];
