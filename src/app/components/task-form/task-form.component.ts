13ae4a4a6272b0d63eeda23d1b3561e35ab9dfb4
Yisus <jesu_ti91@hotmail.com>
2025-03-13 19:29:21 -0500

    Update task-form.component.ts

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -4,29 +4,17 @@ import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-form',
-  templateUrl: './task-form.component.html',
-  styleUrls: ['./task-form.component.css']
+  templateUrl: './task-form.component.html'
 })
 export class TaskFormComponent implements OnInit {
-  taskForm: FormGroup;
 
-  constructor(private taskService: TaskService, private fb: FormBuilder) { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.taskForm = this.fb.group({
-      title: ['', Validators.required],
-      description: ['', Validators.required],
-      timeSpent: ['', Validators.required],
-      rate: ['', Validators.required]
-    });
+
   }
 
   onSubmit(): void {
-    if (this.taskForm.valid) {
-      const task = this.taskForm.value;
-      this.taskService.createTask(task).subscribe(response => {
-        console.log('Task created successfully', response);
-      });
-    }
+
   }
 }
