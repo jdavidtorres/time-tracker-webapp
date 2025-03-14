094668ab9b01ea5e27c16a18f1b99644b32623f0
Yisus <jesu_ti91@hotmail.com>
2025-03-14 11:56:57 -0500

    Fix the build

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -3,18 +3,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TaskService } from '../../services/task.service';
 
 @Component({
-  selector: 'app-task-form',
-  templateUrl: './task-form.component.html'
+	selector: 'app-task-form',
+	templateUrl: './task-form.component.html'
 })
 export class TaskFormComponent implements OnInit {
 
-  constructor(private taskService: TaskService) { }
+	constructor(private taskService: TaskService) { }
 
-  ngOnInit(): void {
+	ngOnInit(): void {
 
-  }
+	}
 
-  onSubmit(): void {
+	onSubmit(): void {
 
-  }
+	}
 }
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -14,7 +14,6 @@ import { Task } from '../../models/Task';
 })
 export class TaskListComponent implements OnInit {
 	tasks: Task[] = [];
-	taskTimers: { [key: number]: any } = {};
 
 	constructor(private taskService: TaskService) { }
 
@@ -30,48 +29,10 @@ export class TaskListComponent implements OnInit {
 		});
 	}
 
-	editTask(task: any): void {
-		this.taskService.updateTask(task).subscribe(response => {
-			const index = this.tasks.findIndex(t => t.id === task.id);
-			if (index !== -1) {
-				this.tasks[index] = response;
-			}
-		});
-	}
-
 	deleteTask(taskId: string): void {
 		this.taskService.deleteTask(taskId).subscribe(() => {
 			this.tasks = this.tasks.filter(task => task.id !== taskId);
 		});
 	}
 
-	startTask(taskId: string): void {
-		if (!this.taskTimers[taskId]) {
-			this.taskTimers[taskId] = { startTime: new Date(), duration: 0 };
-		}
-	}
-
-	pauseTask(taskId: string): void {
-		if (this.taskTimers[taskId]) {
-			const now = new Date();
-			const elapsed = (now.getTime() - this.taskTimers[taskId].startTime.getTime()) / 1000;
-			this.taskTimers[taskId].duration += elapsed;
-			delete this.taskTimers[taskId].startTime;
-		}
-	}
-
-	resumeTask(taskId: string): void {
-		if (this.taskTimers[taskId] && !this.taskTimers[taskId].startTime) {
-			this.taskTimers[taskId].startTime = new Date();
-		}
-	}
-
-	stopTask(taskId: string): void {
-		if (this.taskTimers[taskId]) {
-			const now = new Date();
-			const elapsed = (now.getTime() - this.taskTimers[taskId].startTime.getTime()) / 1000;
-			this.taskTimers[taskId].duration += elapsed;
-			delete this.taskTimers[taskId];
-		}
-	}
 }
